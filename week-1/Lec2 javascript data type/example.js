ef1dc535d23d5025b6a1a1fea6b497c4df2dd081
KUMAR-ABHINAV16 <97382659+KUMAR-ABHINAV16@users.noreply.github.com>
2023-06-24 23:27:52 +0530

    code

---
+++ week-1/Lec2 javascript data type/example.js
@@ -0,0 +1,40 @@
+// intro to strings 
+console.log(typeof ("Hello"));
+let word = 'rain'  //String.Prototy[e]
+// console.log(word.toUpperCase()); //RAINING
+
+
+
+
+console.log("Hello and good evening".length);
+console.log("Hello".charAt(4));
+
+
+// boolean  true =  and false = 0
+
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof isNaN('five'));
+
+
+if ("hello") {
+        console.log(" true");
+} else {
+        console.log("false");
+}
+
+
+console.log("hello".includes('e'));
+
+
+let score1;
+let result1 = null;
+
+// console.log(typeof(score1));
+// console.log(typeof(result1));
+
+if (result1) {
+        console.log("very true");
+} else {
+        console.log("very wrong");
+}
